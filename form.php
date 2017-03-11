7dd9f3bf0d2e5595d161a2c9fff489f4bd4180dc
siddhant034 <shekhar.siddhant@gmail.com>
2017-03-11 08:34:35 +0530

    modified contacts page

--- form.php
+++ form.php
@@ -1,9 +1,9 @@
 <?php
         $email;$comment;$captcha;
         //if(isset($_POST['email'])){
-          //$email=$_POST['email'];
-        //}if(isset($_POST['comment'])){
-         // $email=$_POST['comment'];}
+          //$email=$_POST['email'];}
+          //if(isset($_POST['comment'])){
+         //$email=$_POST['comment'];}
          if(isset($_POST['g-recaptcha-response'])){
           $captcha=$_POST['g-recaptcha-response'];
         }
@@ -15,7 +15,7 @@
         $ip = $_SERVER['REMOTE_ADDR'];
         $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
         $responseKeys = json_decode($response,true);
-        if(intval($responseKeys["success"]) !== 1) {
+        if(intval($responseKeys["success"]) != 1) {
           echo '<h2>You are spammer ! Get the @$%K out</h2>';
         } else {
           echo '<h2>Thanks for posting comment.</h2>';
