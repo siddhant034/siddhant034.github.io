491e239a9a00ef8df50c02e96e62972b38e03909
Siddhant <shekhar.siddhant@gmail.com>
2020-08-29 16:18:10 +0530

    add socket

---
+++ socket/precache-manifest.d8fbca6de075ee0c051b82298d550810.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db63569528daf054982e48050734478",
+    "url": "/Socket-Template/index.html"
+  },
+  {
+    "revision": "5c06dd06e9dda98a8552",
+    "url": "/Socket-Template/static/css/main.1945a1a3.chunk.css"
+  },
+  {
+    "revision": "1b52f2edc3e91d9016a3",
+    "url": "/Socket-Template/static/js/2.164987ed.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/Socket-Template/static/js/2.164987ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c06dd06e9dda98a8552",
+    "url": "/Socket-Template/static/js/main.45aa638b.chunk.js"
+  },
+  {
+    "revision": "822acd6a2e6879c44240",
+    "url": "/Socket-Template/static/js/runtime-main.03c2e7ff.js"
+  }
+]);
